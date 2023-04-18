0a9a38077b86bc008c6b7d963332b755a3281a03
rz871256465 <871256465@qq.com>
2023-04-18 22:13:17 +0100

    change form and create first templates

--- shopping/views.py
+++ shopping/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import shopping_index, Shopping_detail
 # Create your views here.
+
+def shoppingindex(request):
+    shopping_items = shopping_index.objects.all()
+    return render(request, 'shopping/shopping_index.html', {'shopping_items':shopping_items})
+
+def shopping_detail(request):
+    shopping_detail = Shopping_detail.objects.all()
+    return render(request, 'shopping/shopping_detail.html', {'shopping_detail': shopping_detail})
