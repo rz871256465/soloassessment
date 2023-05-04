9c7a655ac6cee160bba22c5e3363d8ddb492ae98
rz871256465 <871256465@qq.com>
2023-05-04 18:24:07 +0100

    change some in view

--- shopping/views.py
+++ shopping/views.py
@@ -120,42 +120,26 @@ def check_by_date(request, date):
 
 def register_view(request):
     if request.method == 'POST':
-        # 检查请求方法是否为 POST
-
         form = UserCreationForm(request.POST)
-        # 根据 UserCreationForm 创建表单实例，并传入 POST 数据
-
         if form.is_valid():
-            # 检查表单数据是否有效
             user = form.save()
-            # 保存用户信息
-
-            # 注册成功后可以执行其他操作，例如自动登录等
-
             return redirect('/')
-            # 返回注册成功后的页面，这里假设 home 是一个 URL 名称
     else:
         form = UserCreationForm()
-        # 若请求方法不是 POST，则创建一个空的 UserCreationForm 实例
-
     return render(request, 'registration/register.html', {'form': form})
-    # 渲染注册页面并传递表单实例给模板
+
 
 def login_view(request):
     if request.method == 'POST':
-        # 处理登录表单的提交
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            # 验证表单数据
             user = form.get_user()
             login(request, user)
-            messages.success(request, '登录成功')
-            return redirect('/')  # 重定向到 index 页面
+            messages.success(request, 'successfully')
+            return redirect('/')
         else:
-            # 登录失败
-            messages.error(request, '登录失败，请检查用户名和密码')
+            messages.error(request, 'error')
     else:
-        # 渲染登录页面
         form = AuthenticationForm(request)
     return render(request, 'registration/login.html', {'form': form})
 
