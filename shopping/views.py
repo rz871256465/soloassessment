c91eff237d97f100e3862d788f5a30d75703d29d
rz871256465 <871256465@qq.com>
2023-05-04 16:00:39 +0100

    delete form.py

--- shopping/views.py
+++ shopping/views.py
@@ -12,6 +12,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 # Create your views here.
 def index(request):
     try:
